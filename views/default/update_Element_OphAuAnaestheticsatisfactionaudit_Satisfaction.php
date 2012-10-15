4aba692d88762d2d3f34c618bff042b5737740f3
splatEric <mike.smith@camc-ltd.co.uk>
2012-10-15 12:34:37 +0100

    + updates to work with the changes to the asset directory structure

--- views/default/update_Element_OphAuAnaestheticsatisfactionaudit_Satisfaction.php
+++ views/default/update_Element_OphAuAnaestheticsatisfactionaudit_Satisfaction.php
@@ -20,7 +20,7 @@
 	<h4 class="elementTypeName"><?php  echo $element->elementType->name; ?></h4>
 
 	<?php echo $form->slider($element, 'pain', array('min' => 0, 'max' => 10, 'step' => 1))?>
-	<div class="eventDetail"><img class="field_key" id="pain_key" src="<?php echo $this->imgPath; if ($this->patient->isChild()) { echo "painscale_child.png"; } else { echo "painscale_adult.png"; } ?>"  /></div>
+	<div class="eventDetail"><img class="field_key" id="pain_key" src="<?php echo $this->assetPath; if ($this->patient->isChild()) { echo "/img/painscale_child.png"; } else { echo "/img/painscale_adult.png"; } ?>"  /></div>
 	<?php echo $form->slider($element, 'nausea', array('min' => 0, 'max' => 4, 'step' => 1))?>
 	<div class="eventDetail"><div class="field_key"><em>0 - none, 1 - mild, 2 - moderate, 3 - severe</em></div></div>
 	<?php echo $form->checkBox($element, 'vomited')?>
