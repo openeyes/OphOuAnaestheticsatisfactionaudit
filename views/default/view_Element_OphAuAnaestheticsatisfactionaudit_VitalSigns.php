f9323daa773541ec79a7a55db5a61c051ce5fbc9
splatEric <mike.smith@camc-ltd.co.uk>
2012-10-16 12:12:41 +0100

    + colour highlight the vital signs for their score in the EWS.

--- views/default/view_Element_OphAuAnaestheticsatisfactionaudit_VitalSigns.php
+++ views/default/view_Element_OphAuAnaestheticsatisfactionaudit_VitalSigns.php
@@ -5,27 +5,27 @@
 	<tbody>
 		<tr>
 			<td width="30%"><?php echo CHtml::encode($element->getAttributeLabel('respiratory_rate_id'))?></td>
-			<td><span class="big"><?php echo $element->respiratory_rate ? $element->respiratory_rate->name : 'None'?></span></td>
+			<td><span class="big score_<?php echo $element->respiratory_rate->score ?>"><?php echo $element->respiratory_rate ? $element->respiratory_rate->name : 'None'?></span></td>
 		</tr>
 				<tr>
 			<td width="30%"><?php echo CHtml::encode($element->getAttributeLabel('oxygen_saturation_id'))?></td>
-			<td><span class="big"><?php echo $element->oxygen_saturation ? $element->oxygen_saturation->name : 'None'?></span></td>
+			<td><span class="big score_<?php echo $element->oxygen_saturation->score ?>"><?php echo $element->oxygen_saturation ? $element->oxygen_saturation->name : 'None'?></span></td>
 		</tr>
 				<tr>
 			<td width="30%"><?php echo CHtml::encode($element->getAttributeLabel('systolic_id'))?></td>
-			<td><span class="big"><?php echo $element->systolic ? $element->systolic->name : 'None'?></span></td>
+			<td><span class="big score_<?php echo $element->systolic->score ?>"><?php echo $element->systolic ? $element->systolic->name : 'None'?></span></td>
 		</tr>
 				<tr>
 			<td width="30%"><?php echo CHtml::encode($element->getAttributeLabel('body_temp_id'))?></td>
-			<td><span class="big"><?php echo $element->body_temp ? $element->body_temp->name : 'None'?></span></td>
+			<td><span class="big score_<?php echo $element->body_temp->score ?>"><?php echo $element->body_temp ? $element->body_temp->name : 'None'?></span></td>
 		</tr>
 				<tr>
 			<td width="30%"><?php echo CHtml::encode($element->getAttributeLabel('heart_rate_id'))?></td>
-			<td><span class="big"><?php echo $element->heart_rate ? $element->heart_rate->name : 'None'?></span></td>
+			<td><span class="big score_<?php echo $element->heart_rate->score ?>"><?php echo $element->heart_rate ? $element->heart_rate->name : 'None'?></span></td>
 		</tr>
 				<tr>
 			<td width="30%"><?php echo CHtml::encode($element->getAttributeLabel('conscious_lvl_id'))?></td>
-			<td><span class="big"><?php echo $element->conscious_lvl ? $element->conscious_lvl->name : 'None'?></span></td>
+			<td><span class="big score_<?php echo $element->conscious_lvl->score ?>"><?php echo $element->conscious_lvl ? $element->conscious_lvl->name : 'None'?></span></td>
 		</tr>
 		<tr <?php if ($element->calculateMEW() > 3) {?>class="highMEWS"<?php } ?>>
 			<td width="30%">Calculated MEWS</td>
