09331d6e668f0bc0badcb21259a6f1800494ab79
abinrax <reda.benjli@openeyes.org.uk>
2013-05-28 14:37:10 +0100

    Revert "OE-2727"
    This reverts commit 829e6b3bac434f4ebc4257584b3d2cf81f1723c0.

--- views/default/view.php
+++ views/default/view.php
@@ -1,17 +1,20 @@
-<?php
-$this->header();
+<?php 	$this->breadcrumbs=array($this->module->id);
+	$this->header();
 ?>
-<h3 class="withEventIcon">
-    <?php echo $this->event_type->name ?>
-</h3> 
- 
+<h3 class="withEventIcon" style="background:transparent url(<?php echo $this->assetPath?>/img/medium.png) center left no-repeat;"><?php  echo $this->event_type->name ?></h3>
 
-    <div id="event_<?php echo $this->module->name ?>">
-        <div id="elements" class="view">
-            <?php $this->renderDefaultElements('view'); ?>
-        </div>
-    </div>
- 
- 
+<div>
+	<?php  $this->renderDefaultElements($this->action->id); ?>	<?php  $this->renderOptionalElements($this->action->id); ?>
+	<div class="metaData">
+		<span class="info">
+			Record created by <span class="user"><?php echo $this->event->user->fullname ?></span> on <?php echo $this->event->NHSDate('created_date') ?> at <?php echo date('H:i', strtotime($this->event->created_date)) ?>
+		</span>
+		<span class="info">
+			Record last modified by <span class="user"><?php echo $this->event->usermodified->fullname ?></span> on <?php echo $this->event->NHSDate('last_modified_date') ?> at <?php echo date('H:i', strtotime($this->event->last_modified_date)) ?>
+		</span>
+	</div>
+	
+	<div class="cleartall"></div>
+</div>
 
-<?php $this->footer(); ?>
+<?php  $this->footer();?>
