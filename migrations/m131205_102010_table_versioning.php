51bde1e5c08b68bb7cbf318e288dd5dbef2a12a1
Mark <m@rkw.me>
2013-12-16 14:00:28 +0000

    [OE-3856] removed unnecessary statements

--- migrations/m131205_102010_table_versioning.php
+++ migrations/m131205_102010_table_versioning.php
@@ -438,29 +438,17 @@ CREATE TABLE `et_ophouanaestheticsataudit_vitalsigns_systolic_version` (
 	public function down()
 	{
 		$this->dropColumn('et_ophouanaestheticsataudit_anaesthetis','deleted');
-		$this->dropColumn('et_ophouanaestheticsataudit_anaesthetis_version','deleted');
 		$this->dropColumn('et_ophouanaestheticsataudit_anaesthetist_lookup','deleted');
-		$this->dropColumn('et_ophouanaestheticsataudit_anaesthetist_lookup_version','deleted');
 		$this->dropColumn('et_ophouanaestheticsataudit_notes','deleted');
-		$this->dropColumn('et_ophouanaestheticsataudit_notes_version','deleted');
 		$this->dropColumn('et_ophouanaestheticsataudit_notes_ready_for_discharge','deleted');
-		$this->dropColumn('et_ophouanaestheticsataudit_notes_ready_for_discharge_version','deleted');
 		$this->dropColumn('et_ophouanaestheticsataudit_satisfactio','deleted');
-		$this->dropColumn('et_ophouanaestheticsataudit_satisfactio_version','deleted');
 		$this->dropColumn('et_ophouanaestheticsataudit_vitalsigns','deleted');
-		$this->dropColumn('et_ophouanaestheticsataudit_vitalsigns_version','deleted');
 		$this->dropColumn('et_ophouanaestheticsataudit_vitalsigns_body_temp','deleted');
-		$this->dropColumn('et_ophouanaestheticsataudit_vitalsigns_body_temp_version','deleted');
 		$this->dropColumn('et_ophouanaestheticsataudit_vitalsigns_conscious_lvl','deleted');
-		$this->dropColumn('et_ophouanaestheticsataudit_vitalsigns_conscious_lvl_version','deleted');
 		$this->dropColumn('et_ophouanaestheticsataudit_vitalsigns_heart_rate','deleted');
-		$this->dropColumn('et_ophouanaestheticsataudit_vitalsigns_heart_rate_version','deleted');
 		$this->dropColumn('et_ophouanaestheticsataudit_vitalsigns_oxygen_saturation','deleted');
-		$this->dropColumn('et_ophouanaestheticsataudit_vitalsigns_oxygen_saturation_version','deleted');
 		$this->dropColumn('et_ophouanaestheticsataudit_vitalsigns_respiratory_rate','deleted');
-		$this->dropColumn('et_ophouanaestheticsataudit_vitalsigns_respiratory_rate_version','deleted');
 		$this->dropColumn('et_ophouanaestheticsataudit_vitalsigns_systolic','deleted');
-		$this->dropColumn('et_ophouanaestheticsataudit_vitalsigns_systolic_version','deleted');
 
 		$this->dropTable('et_ophouanaestheticsataudit_anaesthetis_version');
 		$this->dropTable('et_ophouanaestheticsataudit_anaesthetist_lookup_version');
