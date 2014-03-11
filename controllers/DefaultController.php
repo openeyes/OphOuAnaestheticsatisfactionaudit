fea972ea6a02f3e21bf702788c29c0191c00ef7b
Mark <m@rkw.me>
2014-03-11 14:59:17 +0000

    [OE-3987] removed pointless method definitions

--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -19,28 +19,4 @@
 
 class DefaultController extends BaseEventTypeController
 {
-	protected function beforeAction($action)
-	{
-		return parent::beforeAction($action);
-	}
-
-	public function actionCreate()
-	{
-		parent::actionCreate();
-	}
-
-	public function actionUpdate($id)
-	{
-		parent::actionUpdate($id);
-	}
-
-	public function actionView($id)
-	{
-		parent::actionView($id);
-	}
-
-	public function actionPrint($id)
-	{
-		parent::actionPrint($id);
-	}
 }
